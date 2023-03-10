441bd4fef22228a5bed8aff363ac5d8ea3cc17ca
Ev-geek-niy <ev_geek_niy@vk.com>
2023-03-10 21:20:38 +0500

    Исправлена ошибка с получением названия папки, доработана регулярка
    При получении названия у него могли быть лишние пробелы из-за того, что перед датой создания двойной пробел

--- Xsort/Program.cs
+++ Xsort/Program.cs
@@ -6,7 +6,7 @@ namespace Xsort
     internal class Program
     {
         //language=regex
-        private const string Pattern = " ([0-9]{2,4})(\\.|-)([0-9]{2})(\\.|-)([0-9]{2,4}).+\\.*";
+        private const string Pattern = @" +([0-9]{2,4})(\.|-)([0-9]{2})(\.|-)([0-9]{2,4}).+\.*";
         private static readonly string[] Exts = { ".png", ".jpg", ".mp4" };
         private static readonly string BaseDirectory = Environment.CurrentDirectory;
 
@@ -16,7 +16,7 @@ namespace Xsort
             {
                 foreach (var file in new DirectoryInfo(BaseDirectory).GetFiles().Where(f => Exts.Contains(f.Extension)))
                 {
-                    var folderName = Regex.Replace(file.Name, Pattern, String.Empty);
+                    var folderName = Regex.Replace(file.Name, Pattern, String.Empty).Trim();
                     Directory.CreateDirectory($@"{BaseDirectory}\{folderName}");
                     file.MoveTo($@"{BaseDirectory}\{folderName}\{file.Name}");
                 }
